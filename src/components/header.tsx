d3f2b3afa930e5defbdda7be83c4af13151cc359
Marcos Allan <marcosallanm1to@gmail.com>
2024-12-27 11:20:39 -0300

    feat: component: organization-switcher

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,15 @@
+import { Slash } from 'lucide-react'
 import { ProfileButton } from './profile-button'
+import { OrganizationSwitcher } from './organization-switcher'
 
 export function Header() {
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
-      <div className="flex items-center gap-3">Icon aqui</div>
+      <div className="flex items-center gap-3">
+        Icon aqui
+        <Slash className="size-3 -rotate-[24deg] text-border" />
+        <OrganizationSwitcher />
+      </div>
 
       <div className="flex items-center gap-4">
         <ProfileButton />
