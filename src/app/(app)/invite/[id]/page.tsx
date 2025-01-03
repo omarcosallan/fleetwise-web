78db51e0b4c494bad24803b1760086d0ca1ea21e
Marcos Allan <marcosallanm1to@gmail.com>
2025-01-03 11:14:03 -0300

    feat: deploy config

--- src/app/(app)/invite/[id]/page.tsx
+++ src/app/(app)/invite/[id]/page.tsx
@@ -15,13 +15,11 @@ import { getInvite } from '@/http/get-invite'
 dayjs.extend(relativeTime)
 
 interface InvitePageProps {
-  params: {
-    id: string
-  }
+  params: Promise<{ id: string }>
 }
 
 export default async function InvitePage({ params }: InvitePageProps) {
-  const inviteId = params.id
+  const inviteId = (await params).id
 
   const invite = await getInvite(inviteId)
   const isUserAuthenticated = await isAuthenticated()
--- src/app/(app)/org/[slug]/(org)/update-vehicle.tsx
+++ src/app/(app)/org/[slug]/(org)/update-vehicle.tsx
@@ -1,4 +1,4 @@
-import { VehicleForm, VehicleSchema } from './vehicle-form'
+import { VehicleSchema } from './schemas'
 
 import {
   Sheet,
@@ -11,6 +11,7 @@ import {
 import { Button } from '@/components/ui/button'
 
 import { Ellipsis } from 'lucide-react'
+import { VehicleForm } from './vehicle-form'
 
 export function UpdateVehicle({ initialData }: { initialData: VehicleSchema }) {
   return (
